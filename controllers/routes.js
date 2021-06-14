444a621f50e27216041e4c82c35237f3124cccab
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-06-14 20:45:25 +0530

    Done

--- controllers/routes.js
+++ controllers/routes.js
@@ -2,18 +2,39 @@ const router = require('express').Router()
 const tasks = require('../modals/taskModal');
 
 router.get('/', (req, res) => {
-    res.render('index')
+    var mytasks;
+    tasks.find({}, (err, data) => {
+        if(err){
+            console.log(err);
+        } if (data) {
+            mytasks = data;
+        }
+        res.render('index', { data: mytasks });
+    });
 });
 
 router.post('/add', (req, res) => {
     const task = req.body.task;
-    tasks({ task: task }).save(function(err, doc){
+    tasks({ task: task }).save((err, doc) => {
         if(err){
             console.log(err);
         }
         res.redirect('/')
     })
+});
 
+router.post('/delete', (req, res) => {
+    const id = req.body.id;
+    tasks.findOneAndRemove({ _id: id}, (err, doc) => {
+        res.redirect('/')
+    })
+});
+
+router.post('/update', (req, res) => {
+    const id = req.body.id;
+    tasks.findOneAndUpdate({ _id: id}, { status: true }, (err, doc) => {
+        res.redirect('/')
+    })
 });
 
 
